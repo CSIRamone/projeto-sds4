12ad0c7b33bde7cdd60ce5cd89866feaeb077213
Cidinei Inacio <47647564+CSIRamone@users.noreply.github.com>
2021-09-06 23:29:03 -0300

    Project create

---
+++ backend/src/main/java/com/csiramone/desvenda/DesvendaApplication.java
@@ -0,0 +1,13 @@
+package com.csiramone.desvenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesvendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesvendaApplication.class, args);
+	}
+
+}

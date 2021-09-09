058ffce51ff325cfa02dd5e6de2148b819301bd2
CSIRamone <cidinei_inacio@hotmail.com>
2021-09-08 22:03:47 -0300

    new workspace

---
+++ backend/src/test/java/com/csiramone/dsvendas/DesvendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csiramone.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesvendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
